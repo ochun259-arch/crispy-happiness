cfe4d1035d313d7c33904241074d4de4fb06f2eb
Grow.up <2971528848@qq.com>
2026-03-13 20:33:37 +0800

    三月十三日日报

---
+++ Opencv/Opencv与图像/14.图像噪点处理/3.高斯滤波法.py
@@ -0,0 +1,12 @@
+import cv2
+
+# 1. 读取要滤波的图像
+image_np = cv2.imread('./lena.png')
+
+# 2. 直接进行高斯滤波
+image_Gaussian = cv2.GaussianBlur(image_np, (5, 5), 3)
+
+# 3. 图像显示
+cv2.imshow('image_np', image_np)
+cv2.imshow('image_Gaussian', image_Gaussian)
+cv2.waitKey(0)
